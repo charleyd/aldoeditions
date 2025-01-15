90a83bbda328fc94b5c243f7c1578f772d40a9b3
Charley Dupont <chly.dpnt@gmail.com>
2025-01-15 16:14:47 +0100

    Updating global-scripts

---
+++ global-scripts.js
@@ -0,0 +1,14 @@
+// Lenis
+const lenis = new Lenis({
+  // Plus la valeur est faible, plus le scroll sera fluide
+  lerp: 0.4,
+  // Valeur par défaut : 1
+  // Plus la valeur est haute, plus le défilement sera rapide 
+  wheelMultiplier: 0.9,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
